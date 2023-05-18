ba0935435ff4c2df522010c593a24ebc6e5406a1
Joff Tiquez <jofftiquez@gmail.com>
2023-05-19 01:38:53 +0800

    🎨 style(MainLayout.vue): change background color and add logo to toolbar The background color of the layout has been changed to a semi-transparent blue-green color. A logo has been added to the toolbar, which is now clickable and redirects to the home page. A button has also been added to the toolbar that links to the project's GitHub repository.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,26 @@
 <template>
-  <q-layout view="hHh lpR fFf" style="background: #f2f2f2">
-
+  <q-layout view="hHh lpR fFf" style="background: rgb(189, 233, 239, 0.5)">
     <q-header class="bg-primary text-white">
-      <q-toolbar style="height: 70px;">
-        <q-toolbar-title>
-          Faker.js UI
-        </q-toolbar-title>
+      <q-toolbar style="height: 70px">
+        <q-btn flat no-caps :to="{ name: 'home' }">
+          <img width="50" src="../assets/images/faker-js-ui-logo.png" alt="" />
+          <q-toolbar-title> Faker.js UI </q-toolbar-title>
+        </q-btn>
+        <q-space />
+        <q-btn
+          label="Give us a ⭐ in GitHub"
+          href="https://github.com/jofftiquez/faker-js-ui"
+          target="_blank"
+          size="md"
+          flat
+          no-caps
+        />
+        <!-- <q-btn icon="open_in_new" round flat /> -->
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
