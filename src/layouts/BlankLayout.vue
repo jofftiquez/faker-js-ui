ec946cef47ee98a1683ee70ebb098e6eb0185c11
Joff Tiquez <jofftiquez@gmail.com>
2023-05-19 01:38:39 +0800

    ✨ feat(BlankLayout.vue): add new BlankLayout component This commit adds a new BlankLayout component to the application. The component is a Vue single-file component that defines a layout with a Quasar q-layout component. The layout has a horizontal header, a left panel, a right panel, and a footer. The q-page-container component is used to contain the router-view component. The layout has a semi-transparent background color.

---
+++ src/layouts/BlankLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-layout view="hHh lpR fFf" style="background: rgb(189, 233, 239, 0.5)">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
