df1d0e374b760f3e3e6a1ff411be4453c3bae11c
Joff Tiquez <jofftiquez@gmail.com>
2023-05-19 01:36:08 +0800

    🚀 feat(routes.js): add faker route The faker route is added to the router configuration. The faker route is a child of the MainLayout component and has a path of '/faker'. The component for the faker route is FakerPage.vue.

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,16 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/BlankLayout.vue'),
+    children: [
+      { name: 'home', path: '', component: () => import('pages/IndexPage.vue') },
+    ],
+  },
+  {
+    path: '/faker',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
+      { name: 'faker', path: '', component: () => import('pages/FakerPage.vue') },
     ],
   },
 
