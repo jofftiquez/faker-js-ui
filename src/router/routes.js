80a940b9d1f123b4f5c6989a426c636aaf3b2c1c
Joff Tiquez <jofftiquez@gmail.com>
2023-05-24 23:22:41 +0800

    🔥 refactor(routes.js): remove /faker route The /faker route has been removed from the application as it is no longer needed. This improves the codebase by removing unnecessary code and simplifying the routing configuration.

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,13 +7,6 @@ const routes = [
       { name: 'home', path: '', component: () => import('pages/IndexPage.vue') },
     ],
   },
-  {
-    path: '/faker',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { name: 'faker', path: '', component: () => import('pages/FakerPage.vue') },
-    ],
-  },
 
   // Always leave this as last one,
   // but you can also remove it
