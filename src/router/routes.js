b4c05500a64d0c61521d516d165d9f22b51996c6
Louis <louis.chen2117@gmail.com>
2023-06-25 19:18:13 -0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,18 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { name: 'home', path: '', component: () => import('pages/IndexPage.vue') },
-    ],
-  },
+//     path: '/',
+//     component: () => import('layouts/MainLayout.vue'),
+//     children: [
+//       { name: 'home', path: '', component: () => import('pages/IndexPage.vue') },
+//     ],
+//   },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
-];
+//   // Always leave this as last one,
+//   // but you can also remove it
+//   {
+//     path: '/:catchAll(.*)*',
+//     component: () => import('pages/ErrorNotFound.vue'),
+//   },
+// ];
 
 export default routes;
